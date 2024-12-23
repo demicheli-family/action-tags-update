dee5fbbac9535d967ffbf973a01ff52a55c309d3
Loris Demicheli <lorisdemicheli@gmail.com>
2024-12-23 19:03:09 +0100

    moved git locally

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,30 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import { WorkflowYaml } from './configFile/interfaces';
+import simpleGit, { SimpleGit, SimpleGitFactory, TagResult } from 'simple-git';
 
 async function run() {
   try {
     // Read inputs
-    const token = core.getInput('token', { required: true });
     const filePath = core.getInput('file', { required: true });
     const inputNameTags = core.getInput('input_name_tags') || 'tags';
+    const git: SimpleGit = simpleGit();
+    const tags: TagResult = await git.tags();
 
-    // Create GitHub client
-    const octokit = github.getOctokit(token);
-    const { owner, repo } = github.context.repo;
-
-    // Fetch the latest 10 tags
-    const { data: tags } = await octokit.rest.repos.listTags({
-      owner,
-      repo,
-      per_page: 10
-    });
-
-    const tagNames = tags.map(tag => tag.name);
-
-    // Check if the file exists
     if (!fs.existsSync(filePath)) {
       throw new Error(`File not found: ${filePath}`);
     }
 
-    // Read and parse the existing YAML file
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const yamlData = (yaml.load(fileContent) || {}) as WorkflowYaml;
 
-    // Ensure the structure exists for `on -> workflow_dispatch -> inputs -> [inputNameTags]`
     yamlData.on = yamlData.on || {};
     yamlData.on.workflow_dispatch = yamlData.on.workflow_dispatch || { inputs: {} };
     yamlData.on.workflow_dispatch.inputs = yamlData.on.workflow_dispatch.inputs || {};
     yamlData.on?.workflow_dispatch?.inputs;
-    yamlData.on.workflow_dispatch.inputs[inputNameTags].options = tagNames;
+    yamlData.on.workflow_dispatch.inputs[inputNameTags].options = tags.all.slice(-10);
 
-    // Write the updated YAML back to the file
     const updatedYaml = yaml.dump(yamlData, { noRefs: true });
     fs.writeFileSync(filePath, updatedYaml, 'utf8');
 
